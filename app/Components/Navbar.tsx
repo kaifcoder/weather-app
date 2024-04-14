2a0eede16e55cdefc35e123466f80ddcdf29924c
Mohd Kaif <kaifmohd2014@gmail.com>
2024-04-14 20:58:49 +0530

    Refactor Navbar component and add dynamic rendering of SearchDialog based on current navigation path

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 import { github } from "../utils/Icons";
 import ThemeDropdown from "./ThemeDropdown/ThemeDropdown";
@@ -10,24 +10,16 @@ import { useGlobalContext } from "../context/globalContext";
 function Navbar() {
   const router = useRouter();
   const { state } = useGlobalContext();
+  const navigation = usePathname();
 
   return (
     <div className="w-full py-4 flex items-center justify-between">
       <div className="left"></div>
       <div className="search-container flex shrink-0 w-full gap-2 sm:w-fit">
-        <SearchDialog />
+        {navigation === "/weather" && <SearchDialog />}
 
         <div className="btn-group flex items-center gap-2">
           <ThemeDropdown />
-
-          <Button
-            className="source-code-btn flex items-center gap-2"
-            onClick={() => {
-              router.push("https//github.com");
-            }}
-          >
-            {github} Source Code
-          </Button>
         </div>
       </div>
     </div>
--- app/weather/page.tsx
+++ app/weather/page.tsx
@@ -1,11 +1,10 @@
 "use client";
-import Image from "next/image";
+
 import AirPollution from "../Components/AirPollution/AirPollution";
 import DailyForecast from "../Components/DailyForecast/DailyForecast";
 import FeelsLike from "../Components/FeelsLike/FeelsLike";
 import Humidity from "../Components/Humidity/Humidity";
 import Mapbox from "../Components/Mapbox/Mapbox";
-import Navbar from "../Components/Navbar";
 import Population from "../Components/Population/Population";
 import Pressure from "../Components/Pressure/Pressure";
 import Sunset from "../Components/Sunset/Sunset";
@@ -34,7 +33,9 @@ export default function Home() {
       <div className="pb-4 flex flex-col gap-4 md:flex-row">
         <div className="flex flex-col gap-4 w-full min-w-[18rem] md:w-[35rem]">
           <Temperature />
-          <FiveDayForecast />
+          <div className="max-h-[600px]">
+            <FiveDayForecast />
+          </div>
         </div>
         <div className="flex flex-col w-full">
           <div className="instruments grid h-full gap-4 col-span-full sm-2:col-span-2 lg:grid-cols-3 xl:grid-cols-4">
@@ -43,34 +44,13 @@ export default function Home() {
             <Wind />
             <DailyForecast />
             <UvIndex />
-            <Population />
             <FeelsLike />
             <Humidity />
             <Visibility />
             <Pressure />
           </div>
-          <div className="mapbox-con mt-4 flex gap-4">
+          <div className="mapbox-con mt-4 h-[450px] flex gap-4">
             <Mapbox />
-            <div className="states flex flex-col gap-3 flex-1">
-              <h2 className="flex items-center gap-2 font-medium">
-                Top Large Cities
-              </h2>
-              <div className="flex flex-col gap-4">
-                {defaultStates.map((state, index) => {
-                  return (
-                    <div
-                      key={index}
-                      className="border rounded-lg cursor-pointer dark:bg-dark-grey shadow-sm dark:shadow-none"
-                      onClick={() => {
-                        getClickedCityCords(state.lat, state.lon);
-                      }}
-                    >
-                      <p className="px-6 py-4">{state.name}</p>
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
           </div>
         </div>
       </div>
